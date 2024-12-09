8c61635798578541b46be0ea46d153cc23a2975a
jairom94 <jairom94@gmail.com>
2024-12-09 14:35:02 -0500

    InitiaL commit

---
+++ cifrado_cesar.py
@@ -0,0 +1,33 @@
+from typing import *
+
+ALFABETO = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ '
+LONGITUD_ALFABETO = len(ALFABETO)
+
+def cifrar(mensaje:str,d:int)->str:
+    pasos = 0
+    punto_inicio = 0
+    mensaje_cifrado=''
+    for letra in mensaje.upper():
+        if letra in ALFABETO:
+
+            punto_inicio = ALFABETO.find(letra)
+            pasos = punto_inicio + d
+            pasos = pasos % LONGITUD_ALFABETO
+            mensaje_cifrado += ALFABETO[pasos]
+    return mensaje_cifrado
+
+def creaCifrador(dist:int)->Callable:
+    def cifrador(mensaje: str) -> str:
+        return cifrar(mensaje, dist)
+    return cifrador
+
+def creaParCesar(dist:int)->Tuple[Callable,Callable]:
+    def cifrador(mensaje: str) -> str:
+        return cifrar(mensaje, dist)
+    
+    def descifrador(mensaje: str) -> str:
+        return cifrar(mensaje, -dist)
+
+    return cifrador, descifrador
+
+
---
+++ cifrador_consola.py
@@ -0,0 +1,5 @@
+"""
+Usar las funciones de cifrado cesar 
+en en consola que pida por pantalla 
+el mensaje y la distancia
+"""
